04fa9b12e234d5f9c46f3db4b86c5f3c5a511d7a
Capa-Melenty <cmelenty@ucsd.edu>
2021-02-21 12:03:52 -0800

    I added a button

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
+=======
+// This is the food page
+
+import { useLinkProps } from '@react-navigation/native';
+>>>>>>> Stashed changes
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {Button} from '../components/button';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
@@ -10,8 +17,9 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Welcome Page</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Button/>
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
