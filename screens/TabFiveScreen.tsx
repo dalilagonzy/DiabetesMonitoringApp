dc5dd8a0e29a1910d003240a7591bf06a6601a37
nikitha-vk <nikithakalahasti@gmail.com>
2021-02-28 10:38:40 -0800

    Exercise Page Basic Framework
    I added in the exercise page information and installed the react-native-elemements. I also deleted the text from the buttons component.

--- screens/TabFiveScreen.tsx
+++ screens/TabFiveScreen.tsx
@@ -1,16 +1,60 @@
-// This is the exercise page
-
+import { RevealFromBottomAndroidSpec } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/TransitionSpecs';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, Alert, TextInput } from 'react-native';
+import { initialWindowMetrics } from 'react-native-safe-area-context';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+import{Input} from 'react-native-elements';
+
 export default function TabFiveScreen() {
   return (
+
     <View style={styles.container}>
-      <Text style={styles.title}>Test Tab5</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      
+      <Text style={styles.temp}>Please enter the minutes of exercise </Text>
+
+      <Input 
+      placeholder="" 
+      containerStyle={styles.timeInputContainer}
+      inputStyle={styles.timeInputText}
+      />
+
+      <View style={styles.miniSeparator} />
+
+      <View style={styles.containerForButtons}>
+        <Button
+          title="Easy"
+          color="#DA8EFF"
+          onPress={() => Alert.alert('Easy pressed')}
+        />
+        <Button
+          title="Medium"
+          color="#DA8EFF"
+          onPress={() => Alert.alert('Medium pressed')}
+        />
+        <Button
+          title="Hard"
+          color="#DA8EFF"
+          onPress={() => Alert.alert('Hard pressed')}
+        />
+      </View>
+  
+      <View style={styles.separator} />
+
+      <Input 
+      placeholder="Optional Notes: Describe Exercise" 
+      containerStyle={styles.commentsInputContainer}
+      inputStyle={styles.temp}
+      />
+
+      <Button
+        title="Save"
+        color="#DA8EFF"
+        onPress={() => Alert.alert('Save button pressed')}
+      />
+     
       <EditScreenInfo path="/screens/TabFiveScreen.tsx" />
     </View>
   );
@@ -21,14 +65,48 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    flexDirection: 'column',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  timeInputContainer: {
+    borderRadius: 15,
+    backgroundColor: '#DA8EFF',
   },
+
+  timeInputText: {
+    padding: 10,
+    fontSize: 50,
+    color: '#FFFFFF',
+    alignSelf: 'center',
+  },
+
+  containerForButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
   separator: {
     marginVertical: 30,
-    height: 1,
     width: '80%',
   },
+
+  commentsInputContainer: {
+    padding: 10,
+    borderColor: '#FFFFFF',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+
+  miniSeparator: {
+    marginVertical: 10,
+    width: '80%',
+  },
+
+  temp: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  }
 });
+
+
