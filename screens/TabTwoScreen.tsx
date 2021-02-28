c2f1c7613073f245e9f0c06da9756dfa7cd00fec
Capa-Melenty <cmelenty@ucsd.edu>
2021-02-28 11:43:27 -0800

    I modified the button and added a user input

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,20 +1,29 @@
+
 // This is the food page
 
+import { useLinkProps } from '@react-navigation/native';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,  TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import {Button} from '../components/button';
-import EditScreenInfo from '../components/EditScreenInfo';
+// import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+//import foodTracker from '../components/foodTracker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Food Tracking Page</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
       <Button/>
-    </View>
+      <Input placeholder='BASIC INPUT' />
+      <Input
+   placeholder="Comment"
+   leftIcon={{ type: 'font-awesome', name: 'comment' }}
+   style={textBoxStyle.textBox}/>
+      </View>
     );
 }
 
@@ -34,3 +43,12 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+const textBoxStyle = StyleSheet.create({
+      textBox: {
+         backgroundColor: 'white',
+         borderBottomColor: '#000000',
+         borderBottomWidth: 1, }
+      }
+     )
+
